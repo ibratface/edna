a92d677fc7608f141e715f60af4714fc39490644
Terence So <terence.chang.so@gmail.com>
2022-05-31 21:13:49 +1000

    removed default buckets

--- localstack/init.sh
+++ localstack/init.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# export AWS_ACCESS_KEY_ID=test
-# export AWS_SECRET_ACCESS_KEY=test
-# export AWS_REGION=us-east-1
-
-aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket edna --acl public-read
+# aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket edna --acl public-read
